d9e791626b960c432872f5f3f3dd78d6577093c1
Jayam Malviya <malviya.jayam@gmail.com>
2017-08-19 15:55:12 -0700

    initial commit

---
+++ Utils/MyMath.java
@@ -0,0 +1,17 @@
+package Utils;
+
+
+public class MyMath{
+ public static double multiply(double a, double b){
+ 	return 0;
+ }
+
+ public static double divide(double a, double b){
+ 	return 0;
+ }
+
+
+ public static void main(String[] args){
+
+ }	
+}
