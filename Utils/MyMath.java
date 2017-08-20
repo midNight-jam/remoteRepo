4a5ce007b2cbb3da9359e776ba884f2d4ff850eb
Jayam Malviya <malviya.jayam@gmail.com>
2017-08-19 22:22:00 -0700

    Added the Subtract method

--- Utils/MyMath.java
+++ Utils/MyMath.java
@@ -10,6 +10,15 @@ public class MyMath{
  	return x / y;
  }
 
+ public static double substract(double a, double b){
+ 	return x - y;
+ }
+
+
+ public static double add(double a, double b){
+ 	return 0;
+ }
+
 
  public static void main(String[] args){
 
